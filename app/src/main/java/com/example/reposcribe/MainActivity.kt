fe601e7ece7a5e762beda09d88b6c2b3befbd054
sikap <ksiya1306@gmail.com>
2025-09-14 14:22:56 +0530

    solved dublicate retrofit instance bug

--- app/src/main/java/com/example/reposcribe/MainActivity.kt
+++ app/src/main/java/com/example/reposcribe/MainActivity.kt
@@ -10,7 +10,6 @@ import com.example.reposcribe.navigation.AppNavHost
 import com.example.reposcribe.presentation.screens.SettingsScreen
 import dagger.hilt.android.AndroidEntryPoint
 
-
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
